ab5e81b9398e6409255418f941b6b4d0e14801ea
Kovacs Adela <adela17.21@icloud.com>
2022-11-29 14:59:16 +0200

    laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
 ﻿namespace Kovacs_Adela_lab07;
+using System;
+using Kovacs_Adela_lab07.Data;
+using System.IO;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,35 @@
+using Kovacs_Adela_lab07.Models;
+
+namespace Kovacs_Adela_lab07;
+
+public partial class ListEntryPage : ContentPage
+{
+	public ListEntryPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+    }
+
+}
