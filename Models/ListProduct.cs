8d76281f8b9306e0459781f7c719701a2858e37a
Kovacs Adela <adela17.21@icloud.com>
2022-12-06 18:06:49 +0200

    laborator 9

---
+++ Models/ListProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+
+namespace Kovacs_Adela_lab07.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+
+namespace Kovacs_Adela_lab07.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+
+    }
+}
